5770ae9ad8b66daff9921b83165565448f360dbf
exterminatorz <buit.reinhart@gmail.com>
2025-09-18 21:07:48 +0200

    Update scrypt.go

--- scrypt.go
+++ scrypt.go
@@ -1,8 +1,8 @@
 package main
 
-// This package exists so the module root (github.com/exterminatorz/xk6-crypto-sCrypt)
+// This package exists so the module root (github.com/exterminatorz/xk6-crypto-scrypt)
 // can be imported directly by xk6's --with flag. It blank-imports the real
-// implementation in the `sCrypt` subpackage which performs the actual
+// implementation in the `scrypt` subpackage which performs the actual
 // module registration in its init().
 
 import _ "github.com/exterminatorz/xk6-crypto-scrypt/scrypt"
